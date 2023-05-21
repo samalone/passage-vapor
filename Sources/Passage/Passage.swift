135c6824dc6f18131802bd222e7eb6b25a98c930
Stuart A. Malone <samalone@llamagraphics.com>
2023-05-21 06:48:35 -0400

    Add Application.configure(Passage)

--- Sources/Passage/Passage.swift
+++ Sources/Passage/Passage.swift
@@ -1,8 +1,18 @@
+import Vapor
+import JWT
+
+public enum PassageError: Error {
+    case publicKeyNotBase64
+    case publicKeyInvalidUTF8
+}
+
 public struct Passage {
     public let applicationID: String
+    public let publicKey: String
 
-    public init(appID: String) {
+    public init(appID: String, publicKey: String) throws {
         self.applicationID = appID
+        self.publicKey = publicKey
     }
     
     /// The snippet that you should insert into the body of your HTML to
@@ -13,5 +23,39 @@ public struct Passage {
             <script src="https://psg.so/web.js"></script>
         """
     }
+    
+//    public var signer: JWTSigner {
+//        return JWTSigner.rs256(key: .public(publicKey))
+//    }
 
+}
+
+private enum PassageKey: StorageKey {
+    typealias Value = Passage
+}
+
+extension Application {
+    public var passage: Passage {
+        get {
+            guard let passage = self.storage[PassageKey.self] else {
+                fatalError("Passage not configured. Use app.passage = Passage(...) to configure.")
+            }
+            return passage
+        }
+        set {
+            self.storage[PassageKey.self] = newValue
+        }
+    }
+    
+    public func configure(passage: Passage, isDefault: Bool = true) throws {
+        self.passage = passage
+        guard let data = Data(base64Encoded: passage.publicKey) else {
+            throw PassageError.publicKeyNotBase64
+        }
+        guard let pem = String(data: data, encoding: .utf8) else {
+            throw PassageError.publicKeyInvalidUTF8
+        }
+        let key = try RSAKey.public(pem: pem)
+        self.jwt.signers.use(.rs256(key: key), isDefault: isDefault)
+    }
 }
--- Tests/passage-vaporTests/passage_vaporTests.swift
+++ Tests/passage-vaporTests/passage_vaporTests.swift
@@ -6,7 +6,8 @@ final class passage_vaporTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        let passage = Passage(appID: "KnLF0eFlF4tldSAunTIWQrXq")
+        let passage = Passage(appID: "KnLF0eFlF4tldSAunTIWQrXq",
+                              publicKey: "LS0tLS1CRUdJTiBSU0EgUFVCTElDIEtFWS0tLS0tCk1JSUJDZ0tDQVFFQXl1N3JTMnZFN2pMa0x0a1lRaUtMWStJYjdVMTgwUzQrSlFuQ3NxVFU5WEU1WS9URGYrYlQKWXM4QVVoQW4zYlA1SXpnYU9sRWxzdEh0cE5MZDU3bmZTSUR2MGcraE8yL3c2MGFLdGRDY0F3K2ZIYlRRaGU4OQp4QnkvMW91SkhDQ3U5WVE3UjRQWGZXcUt4NC8reU1xZ3g1aytSbnhxWk5JVDZUSjJFdDVDTEY5STZJMDhjTEVXCllkL29Tc2YyVStKMEE4LzVHYkVSZUYrOW1OQUVHVWNLM095djdMQlB1Y3lyaDV3WTlwQXhyYWVReDlPdnpGdnYKSXpKdEsyWGErNE5hNnhqWHEzNFFXa2Jqc2JXOHBoWjRzaXJtdkNsSXI2TGdsQ1M2UU1zZkJZSCtZZmwva0xSVApvVkNmYVh0RFJ4VmFMdmJWM2FxV2QvMDBVNXdLTFNUTXZ3SURBUUFCCi0tLS0tRU5EIFJTQSBQVUJMSUMgS0VZLS0tLS0K")
         print(passage.authSnippet)
         XCTAssertEqual(passage.authSnippet,
             """
